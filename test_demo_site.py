54cbec7ce640ffc60c4cf61ec956eaa47609cd72
Home pc <mahsumakbas@yahoo.com>
2020-04-12 15:27:50 +0300

    update login test

--- test_demo_site.py
+++ test_demo_site.py
@@ -10,5 +10,12 @@ class TestDemoSite:
         driver.get("http://demo.testpine.com")  
         driver.find_element_by_css_selector("a[name='btnLogin']").click()
         login_form = driver.find_element_by_css_selector("h2.formLabel").text
+        assert login_form == "User Login"
         
-        assert login_form == 'User Login'    
+        driver.find_element_by_id("username").send_keys("admin")
+        driver.find_element_by_id("pwd").send_keys("password")
+        driver.find_element_by_css_selector("button[type='submit']").click()
+        success_txt = driver.find_element_by_css_selector("div.alert > label").text
+        assert success_txt == "Success! You logged in successful."
+        
+        
